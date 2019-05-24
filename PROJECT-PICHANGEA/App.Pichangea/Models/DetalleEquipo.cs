4ee91daee692d10bf955e00d3b3e70395fec4d1c
YONIFTSD <yonathanmc2@gmail.com>
2019-05-24 13:45:53 -0500

    UpdateDetalleEquipo
    UpdateDetalleEquipo

--- PROJECT-PICHANGEA/App.Pichangea/Models/DetalleEquipo.cs
+++ PROJECT-PICHANGEA/App.Pichangea/Models/DetalleEquipo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace App.Pichanguea.Models
 {
+    [Table("DetalleEquipos")]
     public class DetalleEquipo
     {
         public int DetalleEquipoID { get; set; }
